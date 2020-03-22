577d0afe82483152830c1afe07ef68a6dd5248a4
Alexey <valnistuy@gmail.com>
2020-03-22 14:34:13 +0200

    Updates

---
+++ precache-manifest.3f8aeb2c102cfc066de4cefebba057f8.js
@@ -0,0 +1,186 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dad8aa487a89e14de30a06d907c66407",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "8992b911ee4e9600f055",
+    "url": "/goit-react-hw-01-components/static/css/main.93b59262.chunk.css"
+  },
+  {
+    "revision": "3f583b8dbbd66b85788f",
+    "url": "/goit-react-hw-01-components/static/js/2.8a55c355.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goit-react-hw-01-components/static/js/2.8a55c355.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8992b911ee4e9600f055",
+    "url": "/goit-react-hw-01-components/static/js/main.59527652.chunk.js"
+  },
+  {
+    "revision": "2c90fadf0dd2bf61ff2f",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.1ea492af.js"
+  },
+  {
+    "revision": "08eb8e9b6167d23a8a8abdc85999a03b",
+    "url": "/goit-react-hw-01-components/static/media/HelveticaNeueCyrBold.08eb8e9b.woff2"
+  },
+  {
+    "revision": "7588e5c56cce59e04293460483618468",
+    "url": "/goit-react-hw-01-components/static/media/HelveticaNeueCyrBold.7588e5c5.eot"
+  },
+  {
+    "revision": "a4c13254b63ea64451c85d26115432a2",
+    "url": "/goit-react-hw-01-components/static/media/HelveticaNeueCyrBold.a4c13254.woff"
+  },
+  {
+    "revision": "b81a7a2c66608024e5a2cba5f558eefa",
+    "url": "/goit-react-hw-01-components/static/media/HelveticaNeueCyrBold.b81a7a2c.ttf"
+  },
+  {
+    "revision": "cf4836563c37117a090098f0e52455cd",
+    "url": "/goit-react-hw-01-components/static/media/HelveticaNeueCyrBold.cf483656.svg"
+  },
+  {
+    "revision": "3798b0fe2deb1a8c7dd6aaafb2fa557e",
+    "url": "/goit-react-hw-01-components/static/media/HelveticaNeueCyrLight.3798b0fe.svg"
+  },
+  {
+    "revision": "6c47594239b5da19736777db2caa12d5",
+    "url": "/goit-react-hw-01-components/static/media/HelveticaNeueCyrLight.6c475942.eot"
+  },
+  {
+    "revision": "7a6ed3b8e06a9cd8519a62a78be911ea",
+    "url": "/goit-react-hw-01-components/static/media/HelveticaNeueCyrLight.7a6ed3b8.woff2"
+  },
+  {
+    "revision": "b7139790223c74634f6b899d6a478cf8",
+    "url": "/goit-react-hw-01-components/static/media/HelveticaNeueCyrLight.b7139790.woff"
+  },
+  {
+    "revision": "c7f01ac7e6f3c3137e228e5e4ba54703",
+    "url": "/goit-react-hw-01-components/static/media/HelveticaNeueCyrLight.c7f01ac7.ttf"
+  },
+  {
+    "revision": "34325fa6d7bfeff570c8b977ddb4f31a",
+    "url": "/goit-react-hw-01-components/static/media/LatoBlack.34325fa6.svg"
+  },
+  {
+    "revision": "5c856f44a9b1c59665a9a40dcd01a93a",
+    "url": "/goit-react-hw-01-components/static/media/LatoBlack.5c856f44.ttf"
+  },
+  {
+    "revision": "5ccf5798476ec620021a9a4df7a71c4c",
+    "url": "/goit-react-hw-01-components/static/media/LatoBlack.5ccf5798.woff"
+  },
+  {
+    "revision": "f06c626c54b58d10417871f360e7f11d",
+    "url": "/goit-react-hw-01-components/static/media/LatoBlack.f06c626c.woff2"
+  },
+  {
+    "revision": "f3b6cdf99f523f0f2013176cf635674a",
+    "url": "/goit-react-hw-01-components/static/media/LatoBlack.f3b6cdf9.eot"
+  },
+  {
+    "revision": "171943d8174a50c54e0f60b7c4405e7c",
+    "url": "/goit-react-hw-01-components/static/media/LatoBold.171943d8.woff2"
+  },
+  {
+    "revision": "74cfe8679755ac932f4c7b2fbce107f4",
+    "url": "/goit-react-hw-01-components/static/media/LatoBold.74cfe867.woff"
+  },
+  {
+    "revision": "752e106fe89b5139e4c5ea193096afbe",
+    "url": "/goit-react-hw-01-components/static/media/LatoBold.752e106f.eot"
+  },
+  {
+    "revision": "98fadbfaa0efc54d88c8e017c48be5fc",
+    "url": "/goit-react-hw-01-components/static/media/LatoBold.98fadbfa.svg"
+  },
+  {
+    "revision": "9d942e0ff1014c476713a855710ce309",
+    "url": "/goit-react-hw-01-components/static/media/LatoBold.9d942e0f.ttf"
+  },
+  {
+    "revision": "54d0c46c3b1f5642ec24b20d76010e54",
+    "url": "/goit-react-hw-01-components/static/media/LatoRegular.54d0c46c.woff"
+  },
+  {
+    "revision": "626711b6302e38ee954602fa7dc3cfce",
+    "url": "/goit-react-hw-01-components/static/media/LatoRegular.626711b6.svg"
+  },
+  {
+    "revision": "9d3f59642c6f41535b21034da03fd515",
+    "url": "/goit-react-hw-01-components/static/media/LatoRegular.9d3f5964.woff2"
+  },
+  {
+    "revision": "e080040fab017e068ee2901a6cdb5ec4",
+    "url": "/goit-react-hw-01-components/static/media/LatoRegular.e080040f.ttf"
+  },
+  {
+    "revision": "fb89d1ec4a3046f6e500601807adea3a",
+    "url": "/goit-react-hw-01-components/static/media/LatoRegular.fb89d1ec.eot"
+  },
+  {
+    "revision": "4f08e3da1ed8bb521ebffa729817c473",
+    "url": "/goit-react-hw-01-components/static/media/ProximaNovaLight.4f08e3da.woff"
+  },
+  {
+    "revision": "6d37f9ed77a28fb66615cf156e78f2aa",
+    "url": "/goit-react-hw-01-components/static/media/ProximaNovaLight.6d37f9ed.woff2"
+  },
+  {
+    "revision": "90b3680786db32f9b5c112d1537677db",
+    "url": "/goit-react-hw-01-components/static/media/ProximaNovaLight.90b36807.ttf"
+  },
+  {
+    "revision": "a1827f8ed5b3c16f023a41874a33b871",
+    "url": "/goit-react-hw-01-components/static/media/ProximaNovaLight.a1827f8e.eot"
+  },
+  {
+    "revision": "b93ebb97c8405755ad9ec78ccac679b2",
+    "url": "/goit-react-hw-01-components/static/media/ProximaNovaLight.b93ebb97.svg"
+  },
+  {
+    "revision": "06f24d03c3dd3979bcafa4340b09d579",
+    "url": "/goit-react-hw-01-components/static/media/ProximaNovaRegular.06f24d03.ttf"
+  },
+  {
+    "revision": "250e352a058e95549bab4a26ea24b9da",
+    "url": "/goit-react-hw-01-components/static/media/ProximaNovaRegular.250e352a.woff2"
+  },
+  {
+    "revision": "45bae8c7edfb335cf48fdf184faff41e",
+    "url": "/goit-react-hw-01-components/static/media/ProximaNovaRegular.45bae8c7.eot"
+  },
+  {
+    "revision": "c9deb46ff5a81e6c97a39603fe332a87",
+    "url": "/goit-react-hw-01-components/static/media/ProximaNovaRegular.c9deb46f.svg"
+  },
+  {
+    "revision": "e993f94fb236eb17e651fe820da03681",
+    "url": "/goit-react-hw-01-components/static/media/ProximaNovaRegular.e993f94f.woff"
+  },
+  {
+    "revision": "0da042c91b42781e2c497741f0fcad4d",
+    "url": "/goit-react-hw-01-components/static/media/ProximaNovaSemibold.0da042c9.woff2"
+  },
+  {
+    "revision": "461b610b52e5f2020d4450abdbbecb73",
+    "url": "/goit-react-hw-01-components/static/media/ProximaNovaSemibold.461b610b.ttf"
+  },
+  {
+    "revision": "7b3a6f0b1bc29cb5d0b968009f9cf188",
+    "url": "/goit-react-hw-01-components/static/media/ProximaNovaSemibold.7b3a6f0b.eot"
+  },
+  {
+    "revision": "b3c9b5549454f855298bf8a40dced4f5",
+    "url": "/goit-react-hw-01-components/static/media/ProximaNovaSemibold.b3c9b554.woff"
+  },
+  {
+    "revision": "da3016e42483895a03804ba5e8ce0602",
+    "url": "/goit-react-hw-01-components/static/media/ProximaNovaSemibold.da3016e4.svg"
+  }
+]);
